137d15849453a214844f176dd3e5200f341edc84
Mansour Emami <mansuurem@gmail.com>
2026-02-24 19:15:04 +0100

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace smart_handler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("hello word");
+        }
+    }
+}
